b5123d3cedb1e018a7a35ca95bc628b897bbe090
Carlos Andres Rodriguez <cdumar9@hotmail.com>
2017-09-11 23:03:30 -0500

    Se agrega nuevos archivos al repo

---
+++ Gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+
+gulp.task('styles', function() {
+    gulp
+        .src('index.scss')
+        .pipe(sass())
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest('public'));
+})
+
+gulp.task('assets', function() {
+    gulp
+        .src('assets/*')
+        .pipe(gulp.dest('public'));
+});
+/**
+ * 
+ * 
+ * @param {any} watch 
+ */
+function compile(watch) {
+    var bundle = watchify(browserify('./src/index.js'));
+
+    function rebundle() {
+        bundle
+            .transform(babel)
+            .bundle()
+            .pipe(source('index.js'))
+            .pipe(rename('app.js'))
+            .pipe(gulp.dest('public'));
+    }
+    if(watch) {
+        bundle.on('update', function() {
+            console.log('--> Bundling...');
+            rebundle();
+        })
+    }
+
+    rebundle();
+}
+
+gulp.task('scripts', function() {
+    browserify('./src/index.js')
+        .transform(babel)
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'));
+})
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return compile(true); });
+
+gulp.task('default', ['styles', 'assets', 'build']);
